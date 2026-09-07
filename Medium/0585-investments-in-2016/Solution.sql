c676883d7450e75b9f8aa9debe286db11da40fe2
Rudransh <kashyaprudra22@gmail.com>
2026-09-07 15:24:57 +0530

    ✅ [Medium] #0585 - Investments in 2016 | MySQL | Runtime: 749 ms | Memory: 0B

---
+++ Medium/0585-investments-in-2016/Solution.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+
+select round(sum(i.tiv_2016) , 2) as tiv_2016
+from Insurance i
+where (select count(*) from Insurance where tiv_2015 = i.tiv_2015) != 1
+and (select count(*) from Insurance where lat = i.lat and lon = i.lon) = 1;
