68881fa0bc17bc7335126832bf0b763c68749048
Rudransh <kashyaprudra22@gmail.com>
2026-09-07 15:44:54 +0530

    ✅ [Medium] #0039 - Combination Sum | Python3 | Runtime: 4 ms | Memory: 19.4 MB

---
+++ Medium/0039-combination-sum/Solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        
+        def backtrack(remain, start, path):
+            if remain == 0:
+                res.append(list(path))
+                return
+            
+            for i in range(start, len(candidates)):
+                if candidates[i] <= remain:
+                    path.append(candidates[i])
+                    backtrack(remain - candidates[i], i, path)
+                    path.pop()
+                    
+        backtrack(target, 0, [])
+        return res
