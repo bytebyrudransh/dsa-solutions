3dd0b453dc9c99096ac60779a333880d1afaef6f
Rudransh <kashyaprudra22@gmail.com>
2026-09-07 15:31:07 +0530

    ✅ [Medium] #1182 - Game Play Analysis IV | MySQL | Runtime: 1094 ms | Memory: 0B

---
+++ Medium/1182-game-play-analysis-iv/Solution.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT 
+    ROUND(COUNT(t2.player_id) / COUNT(t1.player_id), 2) AS fraction
+FROM 
+    (SELECT player_id, MIN(event_date) AS first_login 
+     FROM Activity 
+     GROUP BY player_id) t1
+LEFT JOIN 
+    Activity t2 
+ON 
+    t1.player_id = t2.player_id 
+    AND t2.event_date = DATE_ADD(t1.first_login, INTERVAL 1 DAY);
