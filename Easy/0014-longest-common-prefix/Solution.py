ab5054123bc3e4d806980c40bca6d7201e64ec88
Rudransh <kashyaprudra22@gmail.com>
2026-09-07 15:55:18 +0530

    ✅ [Easy] #0014 - Longest Common Prefix | Python | Runtime: 24 ms | Memory: 12.4 MB

---
+++ Easy/0014-longest-common-prefix/Solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        if not strs:
+            return ""
+
+        strs.sort()
+        
+        first = strs[0]
+        last = strs[-1]
+        i = 0
+        
+        # Compare characters of the first and last string
+        while i < len(first) and i < len(last) and first[i] == last[i]:
+            i += 1
+            
+        return first[:i]
