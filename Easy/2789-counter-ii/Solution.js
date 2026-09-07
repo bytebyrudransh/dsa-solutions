ecd49f5a3fb0338ab000451cf3bb246c2443c090
Rudransh <kashyaprudra22@gmail.com>
2026-09-07 15:43:55 +0530

    ✅ [Easy] #2789 - Counter II | JavaScript | Runtime: 59 ms | Memory: 58.2 MB

---
+++ Easy/2789-counter-ii/Solution.js
@@ -0,0 +1,9 @@
+var createCounter = function(init) {
+    let current = init;
+    
+    return {
+        increment: () => ++current,
+        decrement: () => --current,
+        reset: () => current = init
+    };
+};
