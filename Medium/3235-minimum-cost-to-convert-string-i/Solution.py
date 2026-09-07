a1fc44ed8821cb2af53af19528434a1cf931bbfc
Rudransh <kashyaprudra22@gmail.com>
2026-09-07 15:25:52 +0530

    ✅ [Medium] #3235 - Minimum Cost to Convert String I | Python | Runtime: 1051 ms | Memory: 22.6 MB

---
+++ Medium/3235-minimum-cost-to-convert-string-i/Solution.py
@@ -0,0 +1,34 @@
+class Solution:
+    def minimumCost(self, source, target, original, changed, cost):
+        inf = float('inf')
+        dist = [[inf] * 26 for _ in range(26)]
+        
+        for i in range(26):
+            dist[i][i] = 0
+            
+        for o, c, z in zip(original, changed, cost):
+            u = ord(o) - ord('a')
+            v = ord(c) - ord('a')
+            dist[u][v] = min(dist[u][v], z)
+            
+        for k in range(26):
+            for i in range(26):
+                for j in range(26):
+                    if dist[i][k] != inf and dist[k][j] != inf:
+                        dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+        
+        total_cost = 0
+        
+        for s_char, t_char in zip(source, target):
+            if s_char == t_char:
+                continue
+            
+            u = ord(s_char) - ord('a')
+            v = ord(t_char) - ord('a')
+            
+            if dist[u][v] == inf:
+                return -1
+            
+            total_cost += dist[u][v]
+            
+        return total_cost
