36c593dcb7825ccab627a19d369b7393d2471fe5
Rudransh <kashyaprudra22@gmail.com>
2026-09-07 15:49:56 +0530

    ✅ [Medium] #1240 - Stone Game II | C++ | Runtime: 9 ms | Memory: 13.6 MB

---
+++ Medium/1240-stone-game-ii/Solution.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int n;
+    vector<int> suffix;
+    vector<vector<int>> dp;
+
+    int solve(int i, int M) {
+        if (i >= n)
+            return 0;
+
+        if (dp[i][M] != -1)
+            return dp[i][M];
+
+        int totalRemaining = suffix[i];
+        int best = 0;
+
+        for (int X = 1; X <= 2 * M && i + X <= n; X++) {
+
+            int opponent = solve(i + X, max(M, X));
+
+            int currentPlayer =
+                totalRemaining - opponent;
+
+            best = max(best, currentPlayer);
+        }
+
+        return dp[i][M] = best;
+    }
+
+    int stoneGameII(vector<int>& piles) {
+        n = piles.size();
+
+        suffix.resize(n + 1, 0);
+
+        for (int i = n - 1; i >= 0; i--) {
+            suffix[i] = suffix[i + 1] + piles[i];
+        }
+
+        dp.assign(n, vector<int>(n + 1, -1));
+
+        return solve(0, 1);
+    }
+};
