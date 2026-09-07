88cb9b39872a557bed1d93373c34efb572e756db
Rudransh <kashyaprudra22@gmail.com>
2026-09-07 15:50:36 +0530

    ✅ [Hard] #0044 - Wildcard Matching | Python | Runtime: 839 ms | Memory: 12.5 MB

---
+++ Hard/0044-wildcard-matching/Solution.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isMatch(self, s, p):
+        m, n = len(s), len(p)
+        prev = [False] * (n + 1)
+        prev[0] = True
+        for j in range(1, n + 1):
+            if p[j - 1] == '*':
+                prev[j] = prev[j - 1]
+            else:
+                break
+        
+        for i in range(1, m + 1):
+            curr = [False] * (n + 1)
+            for j in range(1, n + 1):
+                if p[j - 1] == '*':
+                    curr[j] = curr[j - 1] or prev[j]
+                elif p[j - 1] == '?' or p[j - 1] == s[i - 1]:
+                    curr[j] = prev[j - 1]
+            prev = curr
+        
+        return prev[n]
